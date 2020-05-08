c0e20307bcfd2b0b63f674407a1674963c73355e
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2020-05-08 09:29:10 +0630

    build project and create project

---
+++ SpeechRecognitionTest/SpeechRecognitionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechRecognitionTest
+//
+//  Created by Naw Su Su Nyein on 5/8/20.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
